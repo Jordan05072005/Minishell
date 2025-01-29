8526ac5508f2a449b8adf9622f563757665ef00b
Gulf1603 <hugoleh@icloud.com>
2025-01-29 10:54:30 +0100

    Basic exec works

--- srcs/backend/backend.c
+++ srcs/backend/backend.c
@@ -6,7 +6,7 @@
 /*   By: hle-hena <hle-hena@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 17:35:06 by hle-hena          #+#    #+#             */
-/*   Updated: 2025/01/28 19:18:52 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/01/29 10:49:51 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,39 @@
 int	exec(int nb_cmds, t_cmd *input, char **env)
 {
 	int	pid;
+	int	status;
+	int	exit_code;
 
 	pid = fork();
 	if (pid == -1)
 		ft_perror(1, 0, "A subprocess was not started.");
 	if (pid == 0)
 		exec_proc(env, input, nb_cmds);
+	exit_code = 1;
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+		exit_code = WEXITSTATUS(status);
+	ft_printf("\n\n\nExit status is %d\n", exit_code);
 	return (1);
 }
 
 int	main(int ac, char **av, char **env)
 {
-	t_cmd	input1;
-	char	 *args1[] = {"cat", "-e", NULL};
+	t_cmd	input[2];
+	char	*args1[] = {"ls", "-a", "-R", NULL};
+	char	*args2[] = {"cat", "-e", NULL};
 
-	input1.in = "Makefile";
-	input1.out = NULL;
-	input1.here_doc = "NULL";
-	input1.args = args1;
-	input1.append = 0;
-	exec(1, &input1, env);
+	input[0].in = "Makefile";
+	input[0].out = NULL;
+	input[0].here_doc = NULL;
+	input[0].args = args1;
+	input[0].append = 0;
+	input[1].in = "Makefile";
+	input[1].out = "test";
+	input[1].here_doc = "EOF";
+	input[1].args = args2;
+	input[1].append = 1;
+	exec(2, input, env);
 	(void) ac;
 	(void) av;
 }
--- srcs/backend/io/set_io.c
+++ srcs/backend/io/set_io.c
@@ -6,7 +6,7 @@
 /*   By: hle-hena <hle-hena@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 16:39:04 by hle-hena          #+#    #+#             */
-/*   Updated: 2025/01/28 19:15:06 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/01/29 10:48:53 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,22 @@ void	set_io_cp(int child, int nb_cmds, t_icmd *cmds)
 	{
 		here_doc(cmds[child]);
 	}
-	else if (child == 0 || cmds[child].fd_in != 0)
+	else if (cmds[child].fd_in != 0)
 	{
 		if (dup2(cmds[child].fd_in, 0) == -1)
-			exit(1); //should clean things I think ?
+			ft_perror(1, 0, "File in does not exist\n");
 	}
-	else
+	else if (child > 0)
 	{
-		if (dup2(cmds[child - 1].pipe[0], 0) == -1)
+		if (dup2(cmds[child].pipe[0], 0) == -1)
 			exit(1); //should clean things I think ?
 	}
-	if (child == nb_cmds - 1 || cmds[child].fd_out != 0)
+	if (cmds[child].fd_out != 1)
 	{
 		if (dup2(cmds[child].fd_out, 1) == -1)
-			exit(1); //should clean things I think ?
+			ft_perror(1, 0, "File was not created\n");
 	}
-	else
+	else if (child < nb_cmds - 1)
 	{
 		if (dup2(cmds[child + 1].pipe[1], 1) == -1)
 			exit(1); //should clean things I think ?
