fb494a8f58f3f2e4eb65d26a82e134e8334eee22
Jordan <guaglio.jordan@gmail.com>
2025-01-29 16:15:44 +0100

    ft_strjoin2

--- libft/str/ft_strjoin.c
+++ libft/str/ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hle-hena <hle-hena@students.42perpignan    +#+  +:+       +#+        */
+/*   By: jguaglio <guaglio.jordan@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 11:04:59 by hle-hena          #+#    #+#             */
-/*   Updated: 2024/11/18 18:05:40 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/01/29 16:14:31 by jguaglio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,20 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	ft_strlcpy(dest, s1, s1_len + 1);
 	ft_strlcat(dest, s2, s1_len + s2_len + 1);
 	return (dest);
+}
+
+char	*ft_strjoin2(const char *s1, const char *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*dest;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	dest = ft_calloc(s1_len + s2_len + 1, sizeof(char));
+	if (!dest)
+		return (free(s1), NULL);
+	ft_strlcpy(dest, s1, s1_len + 1);
+	ft_strlcat(dest, s2, s1_len + s2_len + 1);
+	return (free(s1), dest);
 }
