68625cd131de722fe005b745812fa98389b49022
Jordan <guaglio.jordan@gmail.com>
2025-02-04 13:15:30 +0100

    add pwd

--- srcs/exec/exec/exec_builtin.c
+++ srcs/exec/exec/exec_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_builtin.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hle-hena <hle-hena@student.42perpignan.    +#+  +:+       +#+        */
+/*   By: jguaglio <guaglio.jordan@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 13:16:28 by hle-hena          #+#    #+#             */
-/*   Updated: 2025/01/29 15:53:18 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/02/04 13:14:44 by jguaglio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	exec_builtin(t_icmd *cmds, int nb_cmds, int child, char **env)
 	ft_del(cmds[child].path);
 	if (ft_strncmp(cmds[child].args[0], "cd", 3))
 		cmds[child].exit = 0;// ft_cd(cmds[child].args, env)
+	else if (ft_strncmp(cmds[child].args[0], "pwd", 4))
+		cmds[child].exit = ft_pwd(cmds[child].args);
 	else
 		cmds[child].exit = 1;
 }
