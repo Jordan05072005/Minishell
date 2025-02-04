0d72dc77159c0ee4443f14e5a18530f54ac633ab
Jordan <guaglio.jordan@gmail.com>
2025-02-04 13:29:38 +0100

    add unset

---
+++ srcs/data/ft_getloc_struct.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_getloc_struct.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jguaglio <guaglio.jordan@gmail.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 13:28:05 by jguaglio          #+#    #+#             */
+/*   Updated: 2025/02/04 13:28:05 by jguaglio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "env.h"
+
+t_list	*ft_getloc_struct(const char *str)
+{
+	int		i;
+	int		size;
+	char	*line;
+	t_list	loc;
+
+	i = 0;
+	size = ft_strlen(str);
+	loc = data()->loc;
+	line = ft_strnstr(loc.content, str, size);
+	while (!line && loc->next)
+	{
+		loc = loc->next;
+		line = ft_strnstr(loc.content, str, size);
+	}
+	if (!line)
+		return (NULL);
+	return (&loc);
+}
