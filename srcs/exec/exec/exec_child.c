2bede579f6e41085d97570b7662289b0ac4d9919
hle-hena <hugoleh@icloud.com>
2025-02-13 11:24:30 +0100

    Fixed an issue which made a seg possible in exec_child, and updated the search for binary in get_path

--- srcs/exec/exec/exec_child.c
+++ srcs/exec/exec/exec_child.c
@@ -6,7 +6,7 @@
 /*   By: hle-hena <hle-hena@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 17:49:48 by hle-hena          #+#    #+#             */
-/*   Updated: 2025/02/13 10:59:52 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/02/13 11:15:32 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,27 @@
 
 void	exec_child(t_icmd *cmds, int nb_cmds, int child)
 {
+	char	*message;
+
 	set_io_cp(child, nb_cmds, cmds);
 	close_fd(cmds, nb_cmds, child);
 	if (!cmds[child].path)
 	{
 		if (ft_getenv("PATH"))
-			ft_perror(127, ft_strsjoin((const char *[]){cmds[child].args[0],
-					": Command not found.", NULL}),
-				clean_icmds(cmds, nb_cmds) + clean_data());
+		{
+			message = ft_strsjoin((const char *[]){cmds[child].args[0],
+					": Command not found.", NULL});
+			ft_perror(127, message, clean_icmds(cmds, nb_cmds) + clean_data());
+		}
 		else
 			ft_perror(127, NULL, clean_icmds(cmds, nb_cmds) + clean_data());
 	}
 	if (access(cmds[child].path, F_OK | X_OK) != 0)
-		ft_perror(126, ft_strsjoin((const char *[]){"mini: ",
-				cmds[child].path, ": Permission denied.", NULL}),
-			clean_icmds(cmds, nb_cmds) + clean_data());
+	{
+		message = ft_strsjoin((const char *[]){"mini: ",
+				cmds[child].path, ": Permission denied.", NULL});
+		ft_perror(126, message, clean_icmds(cmds, nb_cmds) + clean_data());
+	}
 	if (execve(cmds[child].path, cmds[child].args, env2env(data()->env)) == -1)
 		ft_perror(127, ft_strdup("mini: An error occured during the execution o\
 f the command."), clean_icmds(cmds, nb_cmds) + clean_data());
--- srcs/exec/path/get_path.c
+++ srcs/exec/path/get_path.c
@@ -6,7 +6,7 @@
 /*   By: hle-hena <hle-hena@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 15:41:54 by hle-hena          #+#    #+#             */
-/*   Updated: 2025/02/13 10:36:41 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/02/13 11:24:10 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ char	*get_cmd_path(t_cmd input, t_icmd *cmds, int nb_cmds)
 char	*get_path(t_cmd input, t_icmd *cmd, t_icmd *cmds, int nb_cmds)
 {
 	if (is_builtin(input.args[0]) || (access(input.args[0], F_OK) == 0
-			&& ft_strnstr(input.args[0], "./", ft_strlen(input.args[0]))))
+			&& ft_strnstr(input.args[0], "/", ft_strlen(input.args[0]))))
 		return (ft_strdup(input.args[0]));
 	if (ft_strchr(input.args[0], '='))
 	{
