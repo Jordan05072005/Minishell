db92930af9ad2fc614e8fbff0e6e16dae0d020e3
hle-hena <hugoleh@icloud.com>
2025-02-13 10:05:00 +0100

    Fixed an invalid read error in exec_builtin and added a few more issues in errors.txt

--- srcs/exec/exec/exec_builtin.c
+++ srcs/exec/exec/exec_builtin.c
@@ -6,19 +6,14 @@
 /*   By: hle-hena <hle-hena@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 13:16:28 by hle-hena          #+#    #+#             */
-/*   Updated: 2025/02/12 15:16:11 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/02/13 10:04:04 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
-void	exec_builtin(t_icmd *cmds, int nb_cmds, int child)
+int	switch_cmd(t_icmd *cmds, int child)
 {
-	int	saved[2];
-
-	saved[0] = dup(0);
-	saved[1] = dup(1);
-	set_io_cp(child, nb_cmds, cmds);
 	if (!ft_strncmp(cmds[child].args[0], "cd", 3))
 		cmds[child].exit = ft_cd(cmds[child].args);
 	else if (!ft_strncmp(cmds[child].args[0], "pwd", 4))
@@ -35,8 +30,20 @@ void	exec_builtin(t_icmd *cmds, int nb_cmds, int child)
 		cmds[child].exit = ft_echo(&cmds[child]);
 	else
 		cmds[child].exit = ft_color(cmds[child].args);
+	return (cmds[child].exit);
+}
+
+void	exec_builtin(t_icmd *cmds, int nb_cmds, int child)
+{
+	int	saved[2];
+	int	exit_status;
+
+	saved[0] = dup(0);
+	saved[1] = dup(1);
+	set_io_cp(child, nb_cmds, cmds);
+	exit_status = switch_cmd(cmds, child);
 	dup2(saved[0], 0);
 	dup2(saved[1], 1);
 	if (nb_cmds > 1)
-		return (clean_data(), clean_icmds(cmds, nb_cmds), exit(cmds[child].exit));//This is stupid, access a var after freeing it .............
+		return (clean_data(), clean_icmds(cmds, nb_cmds), exit(exit_status));
 }
