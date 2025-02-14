dbd4d6312ce074701a8ef2eaefd3a6f9ca36ee16
hle-hena <hugoleh@icloud.com>
2025-02-14 18:35:08 +0100

    Fixed issue with cd where if no arguments is passed, ft_strncmp seg. Also fixed the issue of ls -- -a showing the wrong error message.

--- srcs/builtin/cd/cd.c
+++ srcs/builtin/cd/cd.c
@@ -6,13 +6,13 @@
 /*   By: hle-hena <hle-hena@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 14:19:01 by hle-hena          #+#    #+#             */
-/*   Updated: 2025/02/13 22:19:11 by hle-hena         ###   ########.fr       */
+/*   Updated: 2025/02/14 18:34:39 by hle-hena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
-char	*cd_previous(char *arg, char *unused, int *print)
+char	*cd_previous(char *arg, char *unused, int *print, int option)
 {
 	char	*path;
 
@@ -28,19 +28,15 @@ char	*cd_previous(char *arg, char *unused, int *print)
 		*print = 1;
 		return (ft_strdup(path));
 	}
-	if (arg[1] != '-' || ft_strlen(arg) > 2)
+	else if (!option)
 		return (ft_perror(-1, ft_strsjoin((const char *[]){"mini: cd: ", arg, "\
-: invalid option.", NULL}), 0), NULL);
-	path = ft_getenv("HOME");
-	if (!path)
-		path = ft_getloc("HOME");
-	if (!path)
-		return (ft_perror(-1, ft_strdup("mini: cd: HOME is not set."), 0),
-			NULL);
-	return (ft_strdup(path));
+: Invalid option.", NULL}), 0), NULL);
+	else
+		return (ft_perror(-1, ft_strsjoin((const char *[]){"mini: cd: ", arg, "\
+: No such file or directory.", NULL}), 0), NULL);
 }
 
-char	*special_cd(char *arg)
+char	*find_path(char *arg, int *print)
 {
 	char	*path;
 
@@ -67,7 +63,7 @@ char	*special_cd(char *arg)
 	}
 	if (arg[0] == '.')
 		return (create_path(ft_getimp("PWD"), arg));
-	return ("../");
+	return (get_curpath(arg, print));
 }
 
 int	ft_cd(char **av)
@@ -77,17 +73,17 @@ int	ft_cd(char **av)
 	int		print;
 
 	print = 0;
-	option = (ft_strncmp(av[1], "--", 3) == 0);
+	option = 0;
+	if (av[1])
+		option = (ft_strncmp(av[1], "--", 3) == 0);
 	if (ft_strslen(av) > 2 + option)
 		return (ft_perror(-1, ft_strdup("mini: cd: Too many arguments."), 0),
 			1);
-	curpath = special_cd(av[1 + option]);
+	curpath = find_path(av[1 + option], &print);
 	if (!curpath)
 		return (1);
-	else if (!ft_strncmp(curpath, "../", 4))
-		curpath = get_curpath(av[1 + option], &print);
 	if (access(curpath, F_OK) != 0 && av[1 + option][0] == '-')
-		curpath = cd_previous(av[1 + option], curpath, &print);
+		curpath = cd_previous(av[1 + option], curpath, &print, option);
 	if (!curpath)
 		return (1);
 	curpath = check_curpath(curpath, av[1 + option]);
