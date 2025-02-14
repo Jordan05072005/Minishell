e3f25e8dee96cd0a499a40f1116bf4692aa37f94
Jordan <guaglio.jordan@gmail.com>
2025-02-14 11:32:06 +0100

    add export full

--- srcs/builtin/export/export.c
+++ srcs/builtin/export/export.c
@@ -12,29 +12,65 @@
 
 # include "mini.h"
 
-// void	putenv(t_list *d)
-// {
-// 	char	*temp;
-// 	t_list	*cpy;
-// 	int			i;
+int	find_chr(char *str, int ch)
+{
+	int	i;
 
-// 	cpy = d;
-// 	i = 0;
-// 	while (ft_lstsize(cpy) != i)
-// 	{
-// 		temp = cpy->content;
-// 		while (cpy)
-// 		{
-// 			if (ft_strncmp(temp, cpy->content, ft_strlen(temp) + 1) > 0)
-// 		}
-// 	}
-// 	temp = 
-// }
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] == ch)
+			return (i);
+	}
+	return (-1);
+}
+
+t_list	*ft_lstcpy(t_list *d)
+{
+	t_list	*cpy;
+
+	cpy = ft_lstnew(ft_strdup(d->content));
+	d = d->next;
+	while (d && cpy)
+	{
+		ft_lstadd_back(&cpy, ft_lstnew(ft_strdup(d->content)));
+		d = d->next;
+	}
+	return (cpy);
+}
+
+void	putexport(t_list *cpy, int size)
+{
+	t_list	*temp;
+	t_list	*first;
+	char		*temp2;
+	int			i;
+
+	first = cpy;
+	i = -1;
+	while (++i < size)
+	{
+		temp = cpy;
+		while (cpy)
+		{
+			if (!(temp->content) || (cpy->content && ft_strncmp(temp->content, cpy->content, ft_strlen(temp->content) + 1) > 0))
+				temp = cpy;
+			cpy = cpy->next;
+		}
+		temp2 = ft_substr(temp->content, 0, find_chr(temp->content, '='));
+		ft_printf("declare -x %s=\"%s\"\n", temp2, ft_getenv(temp2));
+		ft_del(temp2);
+		ft_del(temp->content);
+		temp->content = NULL;
+		cpy = first;
+	}
+	ft_lstclear(&cpy, ft_del);
+}
 
 int	ft_export(char **arg)
 {
-	t_data *d;
-	int	i;
+	t_data	*d;
+	int			i;
 
 	i = 0;
 	d = data();
@@ -42,14 +78,14 @@ int	ft_export(char **arg)
 	{
 		while (arg[++i])
 		{
-			printf("%s", arg[i]);
+			printf("ici :%s", arg[i]);
 			if (ft_strchr(arg[i], '=') || !ft_getloc(arg[i])) // gèrer t=t===t=t -> t="t===t=t"
 				ft_lstadd_back(&(d->env), ft_lstnew(ft_strdup(arg[i])));
 			else
 				ft_lstadd_back(&d->env, ft_lstnew(ft_strdup(ft_getloc(arg[i]))));
 		}
 	}
-	// else
-	// 	putenv(d);
+	else
+		putexport(ft_lstcpy(d->env), ft_lstsize(d->env));
 	return (0);
 }
